6a623883eee022d3bc2c4b860e935bfefded3f06
louis <vietnp14@gmail.com>
2023-06-20 16:14:40 +0700

    test: Add failed test

--- cypress/e2e/home-page.spec.js
+++ cypress/e2e/home-page.spec.js
@@ -6,4 +6,8 @@ describe('Home page', () => {
   it('should visit app successfully', () => {
     cy.contains('Learn React').should('exist');
   })
+
+  it('should be failed', () => {
+    cy.contains('Failed test').should('exist');
+  })
 });
